1c94f9d498ce57fd03ec7d7da345ca3d13d3377f
alex nham <alexnham11@gmail.com>
2024-02-09 01:32:45 -0500

    project blank

--- src/Projects.js
+++ src/Projects.js
@@ -9,12 +9,12 @@ const Projects = () => {
         <h1 className="text-white pt-10 text-5xl top-0  absolute">Projects</h1>
 
         <div className="w-1/2">
-                <a className="w-full h-full flex justify-center items-center pt-" href="https://project-6olz.onrender.com">
+                <a className="w-full h-full flex justify-center items-center pt-" href="https://project-6olz.onrender.com" target="_blank">
                     <img className="p-10 max-h-full" src={ProjManage} alt="Project Management"></img>
                 </a>
             </div>
             <div className="w-1/2 flex flex-col">
-                <p className="text-white text-4xl pt-36 px-11 flex-1">ProjManage is a website I developed where users are able to display their projects. The web application utilizes React.js for the frontend, Node.js and Express.js for the backend, and MongoDB for the backend. Users are able to create their accounts securely and exhibit their creations! To view my projects <a className="underline text-blue-300 hover:text-blue-800" href="https://project-6olz.onrender.com/user/AlexNham">click here</a></p>
+                <p className="text-white text-4xl pt-36 px-11 flex-1">ProjManage is a website I developed where users are able to display their projects. The web application utilizes React.js for the frontend, Node.js and Express.js for the backend, and MongoDB for the backend. Users are able to create their accounts securely and exhibit their creations! To view my projects <a className="underline text-blue-300 hover:text-blue-800" href="https://project-6olz.onrender.com/user/AlexNham" target="_blank">click here</a></p>
             </div>
            
         </div>
