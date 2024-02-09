4d4f3a0804bba037b73bc8f675e5f1be082fafb1
alex nham <alexnham11@gmail.com>
2024-02-09 11:10:44 -0500

    home background change

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 
 const Home = () => {
     return ( 
-        <div className="bg-green-300 text-white relative bg-contain h-screen w-screen flex justify-center items-center text-5xl">
+        <div className="bg-green-400 text-white relative bg-contain h-screen w-screen flex justify-center items-center text-5xl">
         <h1><a href="#About">Alex Nham</a></h1>
         <a className="absolute top-0 " id="Home"></a>
       </div>
