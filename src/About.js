0cc8e12410d4661bb96aa838c64aa339aea4029d
alex nham <alexnham11@gmail.com>
2024-02-09 10:07:51 -0500

    color change and text fix

--- src/About.js
+++ src/About.js
@@ -4,10 +4,10 @@ import img2 from "./img2.png"
 
 const About = () => {
     return (
-        <div className="relative w-screen h-screen flex flex-row">
-            <div className="bg-black w-3/6 h-screen text-white flex justify-center items-center">
-                <h1 className="absolute pt-10 text-5xl top-10">About Me</h1>
-                <p className="p-32">Ever since I was little, I was always stuck to a computer screen whether it was playing games or watching funny videos. Now I'm stuck to a screen trying to debug code.
+        <div className=" min-w-28 relative w-screen h-screen flex flex-wrap flex-row">
+            <div className="whitespace-normal bg-green-700 w-3/6 h-screen  text-white flex flex-wrap flex-col justify-center items-center">
+                <h1 className=" pt-10 text-5xl top-0">About Me</h1>
+                <p className="overflow-scroll block text-wrap w-3/4 h-3/2 pt-10 ">Ever since I was little, I was always stuck to a computer screen whether it was playing games or watching funny videos. Now I'm stuck to a screen trying to debug code.
                     <br />As an aspiring Software Engineer, I always strive to do better than yesterday. With a solid foundation in Computer Science from the University of Western, where I currently attend, I bring a blend of academic excellence and practical experience to every project. Currently, I'm employed as a Website Coordinator at Gagnon Levesque Inc, where I leverage my skills to develop and enhance client web stores, optimizing workflow processes and delivering top-notch digital experiences.
                     <br />Outside of the digital realm, I love to find ways to better myself, whether it's going to the gym, or even just learning something new. Always finding a way to improve is something I live by.
                     <br /><br />Skills
@@ -18,8 +18,8 @@ const About = () => {
                     <br />  - Project and Time Management Strategic-thinking and Problem-solving abilities
                 </p>
             </div>
-            <div className="bg-white w-3/6 h-screen flex justify-center items-center">
-                <div className="bg-blue-500 w-4/6 h-5/6 flex justify-center items-center">
+            <div className="bg-green-200 w-3/6 h-screen flex justify-center items-center">
+                <div className="bg-green-500 w-4/6 h-5/6 flex justify-center items-center">
                     <img classname="w-full h-full" src={img2}></img>
 
                 </div>
--- src/Projects.js
+++ src/Projects.js
@@ -2,10 +2,10 @@ import ProjManage from "./Logo.png"
 
 const Projects = () => {
     return (  
-        <div className="relative h-screen w-screen bg-white flex justify-center items-center">
+        <div className="relative h-screen w-screen bg-green-400 flex justify-center items-center">
         <a className="absolute top-0" id="Projects"></a>
         
-        <div className="relative bg-black w-5/6 h-5/6 flex justify-center">
+        <div className="relative bg-green-600 w-5/6 h-5/6 flex justify-center">
         <h1 className="text-white pt-10 text-5xl top-0  absolute">Projects</h1>
 
         <div className="w-1/2">
@@ -14,7 +14,7 @@ const Projects = () => {
                 </a>
             </div>
             <div className="w-1/2 flex flex-col">
-                <p className="text-white text-4xl pt-36 px-11 flex-1">ProjManage is a website I developed where users are able to display their projects. The web application utilizes React.js for the frontend, Node.js and Express.js for the backend, and MongoDB for the backend. Users are able to create their accounts securely and exhibit their creations! To view my projects <a className="underline text-blue-300 hover:text-blue-800" href="https://project-6olz.onrender.com/user/AlexNham" target="_blank">click here</a></p>
+                <p className="text-white text-wrap text-2xl pt-36 px-11">ProjManage is a website I developed where users are able to display their projects. The web application utilizes React.js for the frontend, Node.js and Express.js for the backend, and MongoDB for the backend. Users are able to create their accounts securely and exhibit their creations! To view my projects <a className="underline text-blue-300 hover:text-blue-800" href="https://project-6olz.onrender.com/user/AlexNham" target="_blank">click here</a></p>
             </div>
            
         </div>
