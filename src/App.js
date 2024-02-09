e3e09e54f70295a85d636c82f316f54ab730be09
alex nham <alexnham11@gmail.com>
2024-02-08 21:13:04 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="bg-contain h-screen w-screen flex justify-center items-center text-5xl">
+      <h1>Alex Nham</h1>
+    </div>
+  );
+}
+
+export default App;
