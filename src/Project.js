4db88b550a92c1ff1d70654dce92ee59fbda8828
alex nham <alexnham11@gmail.com>
2024-03-11 02:00:09 -0400

    adding all projects

--- src/Project.js
+++ src/Project.js
@@ -1,4 +1,4 @@
-const Project = ({github,projManageLink, image, description}) => {
+const Project = ({github,projManageLink, image, description,link}) => {
     return ( 
         <div className="flex flex-col gap-y-2 w-[240px] h-[340px] rounded-xl bg-[#30323d] pb-5  ">
             
@@ -7,10 +7,11 @@ const Project = ({github,projManageLink, image, description}) => {
             </div>
             
             <div className="flex h-10 w-fit m-auto gap-10 justify-evenly overflow-hidden">
-            <button className='bg-white h-[100%]  px-1 rounded-md' onClick={() => window.open({github})}>Code</button>
-            <button className='bg-white h-[100%] px-1 text-center rounded-md'onClick={() => window.open({projManageLink})}>Demo</button>
+            <button className='bg-white h-[100%]  px-1 rounded-md' onClick={() => window.open(github)}>Code</button>
+            <button className='bg-white h-[100%] px-1 text-center rounded-md' onClick={() => window.open(projManageLink)}>Demo</button>
+            {link && <button className='bg-white h-[100%] px-1 text-center rounded-md' onClick={() => window.open(link)}>Site</button>}
             </div>
-            <div className="text-[10px] p-1 relative h-2/3 w-[90%] m-auto  rounded-lg bg-white break-words">
+            <div className="text-[9px] p-1 relative h-2/3 w-[90%] m-auto  rounded-lg bg-white break-words">
             <p>{description}</p>
             </div>
         </div>
