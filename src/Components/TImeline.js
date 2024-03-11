fbe2fa47f8eab1ee7a1988de8edce42a3a17984b
alex nham <alexnham11@gmail.com>
2024-03-11 14:16:17 -0400

    removing images from timeline

--- src/Components/TImeline.js
+++ src/Components/TImeline.js
@@ -1,21 +1,14 @@
 import React from "react"
 import { Chrono } from "react-chrono";
-import GL from "../Images/GL.png"
 
 const Timeline = () => {
     const items = [
         {
             title: "August 2023",
+            cardTitle: "Gagnon Levesque",
             cardSubtitle: "Website Coordinator",
             cardDetailedText: "As a Website Coordinator, I utilize CSS to enhance client web stores for exceptional digital experiences. I design custom Python automation scripts, improving workflow processes by over 100%. Additionally, I proficiently manage client data with Microsoft Excel, contributing to streamlined operations. My focus on innovation and attention to detail ensures optimal performance and outstanding results.",
-            media: {
-                name: "Gagnon Levesque",
-                source: {
-                    url: `${GL}`
-                },
-                type: "IMAGE"
-            },
-
+          
         },
         {
             title: "March 2024",
@@ -29,10 +22,13 @@ const Timeline = () => {
     return (
         <div className="">
             <Chrono items={items}
+                mediaSettings={{
+                    fit: 'cover',
+                    align: 'center'
+                }}
                 disableToolbar
                 disableClickOnCircle
                 activeItemIndex
-        
                 mode="VERTICAL_ALTERNATING"
                 theme={{
                     titleColor: 'white',
--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -2,13 +2,13 @@ import Timeline from "../Components/TImeline";
 
 const Experience = () => {
     return (
-        <div className="bg-[#30323d] relative justify-center items-center flex flex-col lg:flex-row pb-30">
-        <a className="absolute top-0" id="Projects"></a>
-        <h1 className="text-[#fff] pt-10 text-5xl lg:text-6xl lg:pt-0 lg:pl-10">Experience</h1>
-        <div className="h-full w-full flex justify-center items-center lg:pt-32">
-            <Timeline className="w-full max-w-screen-lg" /> {/* Adjust width as needed */}
-        </div>
+       <div className="bg-[#30323d] relative justify-center items-center flex flex-col pt-10 pb-30">
+    <a className="absolute top-0" id="Projects"></a>
+    <h1 className="text-[#fff] pt-10 text-5xl lg:text-6xl lg:pt-0 lg:pl-10">Experience</h1>
+    <div className="h-full w-full flex justify-center items-center lg:pt-32">
+        <Timeline className="w-full max-w-screen-lg" /> {/* Adjust width as needed */}
     </div>
+</div>
 
     );
 }
