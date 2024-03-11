983d557ac46661c9ed7377e80128330431d43ad1
alex nham <alexnham11@gmail.com>
2024-03-11 14:26:49 -0400

    removing page on timeline

--- src/Components/TImeline.js
+++ src/Components/TImeline.js
@@ -9,14 +9,7 @@ const Timeline = () => {
             cardSubtitle: "Website Coordinator",
             cardDetailedText: "As a Website Coordinator, I utilize CSS to enhance client web stores for exceptional digital experiences. I design custom Python automation scripts, improving workflow processes by over 100%. Additionally, I proficiently manage client data with Microsoft Excel, contributing to streamlined operations. My focus on innovation and attention to detail ensures optimal performance and outstanding results.",
           
-        },
-        {
-            title: "March 2024",
-            cardTitle: "S & P Agricultural Services",
-            cardSubtitle: "Web Developer",
-            cardDetailedText: "Developed and launched the company website, achieving a modern and user-friendly online presence. Conduct regular updates and enhancements to maintain optimal performance and usability. Utilize React, Tailwind, and other web development technologies to craft engaging and responsive user interfaces. Perform ongoing maintenance and troubleshooting to address technical issues and enhance site functionality.",
-
-        },
+        }
     ];
 
     return (
