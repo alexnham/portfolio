a7e60fc622f892abc3bd3199b2b2729f54bcd832
alex nham <alexnham11@gmail.com>
2024-07-22 18:40:07 -0400

    updated experience

--- src/Components/TImeline.js
+++ src/Components/TImeline.js
@@ -1,23 +1,24 @@
 import React from "react"
 import { Chrono } from "react-chrono";
 
-const Timeline = () => {
-    const items = [
-        {
-            title: "August 2023",
-            cardTitle: "Gagnon Levesque",
-            cardSubtitle: "Website Coordinator",
-            cardDetailedText: "As a Website Coordinator, I utilize CSS to enhance client web stores for exceptional digital experiences. I design custom Python automation scripts, improving workflow processes by over 100%. Additionally, I proficiently manage client data with Microsoft Excel, contributing to streamlined operations. My focus on innovation and attention to detail ensures optimal performance and outstanding results.",
-          
-        },
-        {
-            title: "April 2024",
-            cardTitle: "AQX AI",
-            cardSubtitle: "Software Developer Intern",    
-            cardDetailedText: "TBD",      
-        }
-    ];
+const items = [
+    {
+        title: "August 2023",
+        cardTitle: "Gagnon Levesque",
+        cardSubtitle: "Website Coordinator",
+        cardDetailedText: "As a Website Coordinator, I utilize CSS to enhance client web stores for exceptional digital experiences. I design custom Python automation scripts, improving workflow processes by over 100%. Additionally, I proficiently manage client data with Microsoft Excel, contributing to streamlined operations. My focus on innovation and attention to detail ensures optimal performance and outstanding results.",
+      
+    },
+    {
+        title: "April 2024",
+        cardTitle: "AQX AI",
+        cardSubtitle: "Software Developer Intern",    
+        cardDetailedText: "In this role, I developed and optimized a real-time communication server integrated with AI, significantly reducing latency by over 90%. I integrated various speech-to-text and text-to-speech solutions, designed a user-friendly interface for initiating outbound calls, and enhanced system performance by 50%. Additionally, I conducted extensive testing on Azure cloud services to improve scalability and integration.",      
+    }
+];
 
+const Timeline = () => {
+   
     return (
         <div className="">
             <Chrono items={items}
